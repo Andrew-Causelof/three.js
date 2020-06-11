d6326ea2e2f19dac052ee3d17866a65958581e65
Andrey <44496797+Andrew-Causelof@users.noreply.github.com>
2020-06-12 01:05:40 +0300

    finished sprites for the human spine

--- SPINLIFE/script.js
+++ SPINLIFE/script.js
@@ -1,7 +1,7 @@
 var container, controls;
 var camera, scene, renderer;
 
-// so it needs to do configurator right here 
+// so defenetly it needs to do configurator right here 
 var pmremGenerator, envMap;  // clinic environement - global variables
 var absorberColor = new THREE.MeshMatcapMaterial({ color: 0x178FFF });
 //var brainColor = new THREE.MeshMatcapMaterial({ color: 0x660000 });
@@ -10,19 +10,27 @@ var absorberColor = new THREE.MeshMatcapMaterial({ color: 0x178FFF });
 //var bones, absorbers, cord;
 var  absorbers, cord;
 var lumbar, thoracic, cervical, sacrum;  
-
+//
+var c_sprite, t_sprite, l_sprite, s_sprite;
 //
 container = document.createElement( 'div' );
 document.body.appendChild( container );
 //
 
 camera = new THREE.PerspectiveCamera( 50, window.innerWidth / window.innerHeight, 0.25, 300 );
-camera.position.set( - 30, 20, 20 );
+//camera.position.set( - 30, 20, 20 );  // common view
+ camera.position.set( 0, 0, 40 ); //use for sprites only
+//camera.rotation.set(0, 0, 0);
+
+console.log(camera);
 
 scene = new THREE.Scene();
 
 scene.background = new THREE.Color('white');
-
+/*
+var helper = new THREE.CameraHelper( camera );
+scene.add( helper );
+*/
 
 
 
@@ -52,8 +60,8 @@ function init() {
 	controls.minDistance = 0.1;
 	controls.maxDistance = 100;
     controls.target.set( 0, -1, 0 );
-	controls.update();
-
+    controls.update();
+    
 	window.addEventListener( 'resize', onWindowResize, false );
 
 }
@@ -154,6 +162,36 @@ function loader() {
         scene.add( cord );
         render();	
     } );
+
+    //--------------custom-----------------------------
+    
+    c_sprite = makeTextSprite( " Шейный отдел ", 
+    { fontsize: 20, borderColor: {r:0, g:0, b:0, a:1.0}, backgroundColor: {r:100, g:100, b:100, a:0} } );
+    c_sprite.scale.set(15, 11, 0);
+    c_sprite.position.set(9,7,1);
+    scene.add( c_sprite );
+    //----------------------------------------
+    t_sprite = makeTextSprite( " Грудной отдел ", 
+    { fontsize: 20, borderColor: {r:0, g:0, b:0, a:1.0}, backgroundColor: {r:100, g:100, b:100, a:0} } );
+    t_sprite.scale.set(15, 11, 0);
+    t_sprite.position.set(11,-2,1);
+    scene.add( t_sprite );
+    //--------------------------------------------------
+    l_sprite = makeTextSprite( " Поясничный отдел ", 
+    { fontsize: 20, borderColor: {r:0, g:0, b:0, a:1.0}, backgroundColor: {r:100, g:100, b:100, a:0} } );
+    l_sprite.scale.set(15, 11, 0);
+    l_sprite.position.set(9,-14,1);
+    scene.add( l_sprite );
+
+    //--------------------------------------------------
+    s_sprite = makeTextSprite( " Крестцовый отдел ", 
+    { fontsize: 20, borderColor: {r:0, g:0, b:0, a:1.0}, backgroundColor: {r:100, g:100, b:100, a:0} } );
+    s_sprite.scale.set(15, 11, 0);
+    s_sprite.position.set(12,-19,1);
+    scene.add( s_sprite );
+    //--------------------------------------------------
+
+    spriteVisible(false);
 }
 
   function Lighting() {
@@ -165,19 +203,38 @@ function loader() {
         camera.add( pointLight );
   }
 
-  function spineLabling() {
-
-  }
-
-
 function range() {
 
+    
+
     var range = document.getElementById('range');
+
+    console.log(range.value);
+
     var sliding = range.value / 15;
 
-    range.value < 40 ? absorbers.visible = false : absorbers.visible = true;
-    range.value < 25 ? bonesVisible(false) : bonesVisible(true);
+    if (range.value > 40 && range.value < 60) {
+        bonesMoving(0);
+        absorbers.position.x = 0;
+        cord.position.x = 0;
 
+        spriteVisible(false);
+        bonesVisible(true); 
+        absorbers.visible = true; 
+        cord.visible      = true;
+    }
+
+    if (range.value < 40 && range.value > 25) {
+        absorbers.visible = false;
+        cord.visible      = true;
+        spriteVisible(false);
+        bonesVisible(true);    
+
+        bonesMoving(0);
+        absorbers.position.x = 0;
+        cord.position.x      = 0;
+    }
+ 
     if (range.value > 15 && range.value <= 25 ){
 
         bonesMoving(0);
@@ -187,32 +244,38 @@ function range() {
         cord.visible      = true;
         absorbers.visible = false;
         bonesVisible(false); 
+        spriteVisible(false);
 
     }
 
     if (range.value <= 15) {
 
-        bonesVisible(true); 
-
+        bonesVisible(true);
         cord.visible = false;
-        cervical.position.x = -2;
-        thoracic.position.x = 2;
-        lumbar.position.x   = -2;
-        sacrum.position.x   = 2;
+        absorbers.visible = false;
+
+        spriteVisible(true); 
+
+        cervical.position.x = -1;
+        thoracic.position.x = 1;
+        lumbar.position.x   = -1;
+        sacrum.position.x   = 1;
         
     }
 
 
     if (range.value > 60 ) {
+
+        spriteVisible(false); 
+        bonesVisible(true); 
+        absorbers.visible = true; 
+        cord.visible      = true;
+
         bonesMoving(range.value / 8 - sliding);
         absorbers.position.x = range.value / 18 - sliding;
         cord.position.x = - sliding;
     }
-    if (range.value > 40 && range.value < 60) {
-        bonesMoving(0);
-        absorbers.position.x = 0;
-        cord.position.x = 0;
-    }
+
     render();
 }
 
@@ -240,6 +303,37 @@ function bonesMoving(x) {
     render();
 }
 
+function spriteVisible(boolean) {
+    if (boolean == false) {
+        c_sprite.visible  = false;
+        t_sprite.visible  = false;
+        l_sprite.visible  = false;
+        s_sprite.visible  = false;
+    } else {
+        c_sprite.visible  = true;
+        t_sprite.visible  = true;
+        l_sprite.visible  = true;
+        s_sprite.visible  = true;
+    }
+}
+
+// -  - - - - - - - Creating Lable - - - - - - - - - - - - - - - 
+
+
+
+// -  - - - - - - - END Lable Block - - - - - - - - - - - - - - - 
+function showingSprites() {
+
+    /*
+    var spriteMap = new THREE.TextureLoader().load( "sprite.png" );
+    var spriteMaterial = new THREE.SpriteMaterial( { map: spriteMap } );
+    var sprite = new THREE.Sprite( spriteMaterial );
+    sprite.scale.set(5, 5, 5);
+    scene.add( sprite );
+*/
+  //  console.log(sprite);
+    render();
+}
 
 
 //------------- making screenshot -------------------
@@ -265,3 +359,76 @@ const elem = document.querySelector('#screenshot');
     };
   }());
 //---------------------------------------------------------
+
+function makeTextSprite( message, parameters )
+{
+	if ( parameters === undefined ) parameters = {};
+	
+	var fontface = parameters.hasOwnProperty("fontface") ? 
+		parameters["fontface"] : "Arial";
+	
+	var fontsize = parameters.hasOwnProperty("fontsize") ? 
+		parameters["fontsize"] : 18;
+	
+	var borderThickness = parameters.hasOwnProperty("borderThickness") ? 
+		parameters["borderThickness"] : 4;
+	
+	var borderColor = parameters.hasOwnProperty("borderColor") ?
+		parameters["borderColor"] : { r:0, g:0, b:0, a:1.0 };
+	
+	var backgroundColor = parameters.hasOwnProperty("backgroundColor") ?
+		parameters["backgroundColor"] : { r:255, g:255, b:255, a:1.0 };
+
+	var spriteAlignment = THREE.SpriteAlignment;
+		
+	var canvas = document.createElement('canvas');
+	var context = canvas.getContext('2d');
+	context.font = "Bold " + fontsize + "px " + fontface;
+    
+	// get size data (height depends only on font size)
+	var metrics = context.measureText( message );
+	var textWidth = metrics.width;
+	
+	// background color
+	context.fillStyle   = "rgba(" + backgroundColor.r + "," + backgroundColor.g + ","
+								  + backgroundColor.b + "," + backgroundColor.a + ")";
+	// border color
+	context.strokeStyle = "rgba(" + borderColor.r + "," + borderColor.g + ","
+								  + borderColor.b + "," + borderColor.a + ")";
+
+	context.lineWidth = borderThickness;
+	roundRect(context, borderThickness/2, borderThickness/2, textWidth + borderThickness, fontsize * 1.4 + borderThickness, 6);
+	// 1.4 is extra height factor for text below baseline: g,j,p,q.
+	
+	// text color
+	context.fillStyle = "rgba(0, 0, 0, 1.0)";
+
+	context.fillText( message, borderThickness, fontsize + borderThickness);
+	
+	// canvas contents will be used for a texture
+	var texture = new THREE.Texture(canvas) 
+	texture.needsUpdate = true;
+
+	var spriteMaterial = new THREE.SpriteMaterial( 
+		{ map: texture, useScreenCoordinates: false, alignment: spriteAlignment } );
+	var sprite = new THREE.Sprite( spriteMaterial );
+	sprite.scale.set(100,50,1.0);
+	return sprite;	
+}
+
+function roundRect(ctx, x, y, w, h, r) 
+{
+    ctx.beginPath();
+    ctx.moveTo(x+r, y);
+    ctx.lineTo(x+w-r, y);
+    ctx.quadraticCurveTo(x+w, y, x+w, y+r);
+    ctx.lineTo(x+w, y+h-r);
+    ctx.quadraticCurveTo(x+w, y+h, x+w-r, y+h);
+    ctx.lineTo(x+r, y+h);
+    ctx.quadraticCurveTo(x, y+h, x, y+h-r);
+    ctx.lineTo(x, y+r);
+    ctx.quadraticCurveTo(x, y, x+r, y);
+    ctx.closePath();
+    ctx.fill();
+	ctx.stroke();   
+}
